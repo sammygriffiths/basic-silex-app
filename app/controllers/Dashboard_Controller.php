7fe3345e5fe2603e57b3cc5f66e606e69d23a831
Sammy Griffiths <sammy@sammygriffiths.co.uk>
2017-06-10 14:36:38 +0100

    Fix a few issues with install

--- app/controllers/Dashboard_Controller.php
+++ app/controllers/Dashboard_Controller.php
@@ -2,13 +2,11 @@
 
 namespace Griff;
 
-use Symfony\Component\HttpFoundation\Request;
-
 class Dashboard_Controller extends Core_Controller
 {
 
-    public function index(Request $request, Application $app) {
-        return $app['twig']->render('dashboard.html.twig');
+    public function index() {
+        return $this->app['twig']->render('dashboard.html.twig');
     }
 
 }
